5e73b327fc423d91552fccaa18aeb2d3cba00547
Adolfonator182 <a620014428@my.uvm.edu.mx>
2024-12-05 14:14:01 -0600

    Add files via upload

--- models/contacto.js
+++ models/contacto.js
@@ -20,7 +20,7 @@ contactSchema.set('toJSON', {
     transform:(document, returnedObject)=>{
         returnedObject.id=returnedObject._id.toString()
         delete returnedObject._id
-        delete returnedObject._v
+        delete returnedObject.__v
     }
 })
 module.exports = mongoose.model('Contact', contactSchema)
